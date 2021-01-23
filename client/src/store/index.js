cd8e8abab90194138c2eff34b21425aab0ce7f10
Yangrongrui He <frankhe@yangrongruismbp.home>
2021-01-23 17:47:17 -0500

    Debug redux

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,41 +1,47 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import { createLogger } from "redux-logger";
-import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+// import { createLogger } from "redux-logger";
+// import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
 //Redux set up
+const initialState = {};
 const middlewares = [thunk];
 
-if (process.env.NODE_ENV === "development") {
-  const logger = createLogger({
-    collapsed: true,
-  });
-  middlewares.push(logger);
-  middlewares.push(reduxImmutableStateInvariant());
-}
+// if (process.env.NODE_ENV === "development") {
+//   const logger = createLogger({
+//     collapsed: true,
+//   });
+//   middlewares.push(logger);
+//   middlewares.push(reduxImmutableStateInvariant());
+// }
+const store = createStore(rootReducer, initialState, compose(
+  applyMiddleware(...middlewares),
+  window.__REDUX_DEVTOOLS_EXTENSION__
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : f => f
+));
+// function configureStore(initialState) {
 
-function configureStore(initialState) {
+//   //******COMMENT OUT FOR DEPLOYMENT*********//
+//   // const composeEnhancers =
+//   //   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
+//   //*****************************************//
 
-  //******COMMENT OUT FOR DEPLOYMENT*********//
-  // const composeEnhancers =
-  //   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
-  //*****************************************//
+//   //*******COMMENT OUT FOR DEVELOPMENT********//
+//   // const middlewareEnhancer = applyMiddleware(...middlewares); // add support for Redux dev tools
+//   //*****************************************//
 
-  //*******COMMENT OUT FOR DEVELOPMENT********//
-  // const middlewareEnhancer = applyMiddleware(...middlewares); // add support for Redux dev tools
-  //*****************************************//
+//   return createStore(
+//     rootReducer,
+//     initialState,
+//     compose(
+//       applyMiddleware(...middlewares),
+//       window.__REDUX_DEVTOOLS_EXTENSION__
+//         ? window.__REDUX_DEVTOOLS_EXTENSION__()
+//         : f => f
+//     )
+//   );
+// }
 
-  return createStore(
-    rootReducer,
-    initialState,
-    compose(
-      applyMiddleware(...middlewares),
-      window.__REDUX_DEVTOOLS_EXTENSION__
-        ? window.__REDUX_DEVTOOLS_EXTENSION__()
-        : f => f
-    )
-  );
-}
-
-export default configureStore();
+export default store;
